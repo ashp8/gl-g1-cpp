4a6272681ca01a69cf0ee86476751c1f4b58dd1d
ashp8 <ashishira521@gmail.com>
2023-02-05 16:37:22 +0600

    glfw added.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    GLFWwindow  *window;
+
+    if(!glfwInit()){
+        exit(EXIT_FAILURE);
+    }
+    window = glfwCreateWindow(640, 480, "Example", NULL, NULL);
+    if(!window){
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    while(!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
     return 0;
 }
